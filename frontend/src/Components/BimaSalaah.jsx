54245817c0c3f6d86111af9f85d2b26de6582633
Himanshu gaur <himanshugaur055@gmail.com>
2025-10-16 16:36:40 +0530

    section

--- frontend/src/Components/BimaSalaah.jsx
+++ frontend/src/Components/BimaSalaah.jsx
@@ -4,18 +4,18 @@ import { Briefcase, FileText, Zap } from 'lucide-react';
 
 const BimaSalaah = () => {
     return (
-        <section className="bg-gray-50 py-16 sm:py-20">
-            <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8">
-                <div className="grid grid-cols-1 md:grid-cols-2 gap-12 items-center">
+        <section className="bg-gray-50  py-16 sm:py-20">
+            <div className="max-w-6xl  mx-auto bg-blue-800 rounded-xl px-4 sm:px-6 lg:px-8">
+                <div className="grid grid-cols-1  md:grid-cols-2 gap-12 items-start">
                     {/* Left Image */}
-                    <div className="sticky top-20">
+                    <div className="md:sticky mt-4 top-20">
                         <img src="https://images.bimakavach.com/v4/homepage/BimaSalaah_Img.webp" alt="Bima Salaah" className="rounded-lg  w-full h-auto shadow-lg" />
                     </div>
 
                     {/* Right Content */}
                     <div>
-                        <div className="mb-8">
-                            <h2 className="text-4xl font-bold text-indigo-900">Bima Salaah</h2>
+                        <div className="mb-8 mt-4">
+                            <h2 className="text-4xl font-bold text-white bold">Bima  <span className='italic text-yellow-400'>Salaah </span></h2>
                             <p className="mt-2 text-xl text-indigo-600 font-semibold">Free AI-Powered Insurance Advice</p>
                         </div>
                         <div className="space-y-8">
