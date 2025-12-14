7571edb79396d635adef37efefd70dc4236f5809
Himanshu gaur <himanshugaur055@gmail.com>
2025-12-14 20:18:25 +0530

    final social media added

--- frontend/src/Components/SocialHandles.jsx
+++ frontend/src/Components/SocialHandles.jsx
@@ -3,11 +3,11 @@ import { Facebook, Instagram, Twitter } from 'lucide-react';
 
 export default function SocialHandles() {
   return (
-    <div className="fixed bottom-4 right-4 flex flex-col gap-4">
-      <a href="https://www.facebook.com/share/17vAbA1HH6/" target="_blank" rel="noopener noreferrer" className="bg-blue-600 text-white p-3 rounded-full shadow-lg hover:bg-blue-700 transition">
+    <div className="fixed bottom-4 right-4 flex flex-col z-50 gap-4">
+      <a href="https://www.facebook.com/share/17vAbA1HH6/" target="_blank" rel="noopener noreferrer" className="bg-blue-600 text-white p-3 cursor-pointer rounded-full shadow-lg hover:bg-blue-700 transition">
         <Facebook size={24} stroke="currentColor" />
       </a>
-      <a href="https://twitter.com/" target="_blank" rel="noopener noreferrer" className="bg-red-400 text-white p-3 rounded-full shadow-lg hover:bg-blue-500 transition">
+      <a href="https://www.instagram.com/smrf.inserv?igsh=NTE0MnY5MHYycXV4" target="_blank" rel="noopener noreferrer" className="bg-red-400 text-white p-3 rounded-full shadow-lg hover:bg-red-500 transition">
         <Instagram size={24} stroke="currentColor" />
       </a>
     </div>
