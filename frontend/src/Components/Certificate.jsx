80fa5fca9dfc2bc4d8abd99022be9ebcac27e36d
Himanshu gaur <himanshugaur055@gmail.com>
2026-01-16 15:51:22 +0530

    certificate added

--- frontend/src/Components/Certificate.jsx
+++ frontend/src/Components/Certificate.jsx
@@ -3,8 +3,8 @@ import React from "react";
 const Certificate = () => {
   return (
     <section className="bg-gray-50 py-12">
-      <div className="max-w-5xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="bg-white p-8 rounded-xl border-2 border-blue-500">
+      <div className="max-w-5xl mx-auto px-2 sm:px-6 lg:px-8">
+        <div className="bg-white p-2 rounded-xl border-2 border-blue-500">
             <div className="grid md:grid-cols-1 gap-12 items-center">
                 {/* Image */}
                 <div className="text-center">
